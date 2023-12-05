1577bac6bcae42610c7b04afca4e4c9edce76eb0
NGUYEN VAN THANG <102210307@sv1.dut.udn.vn>
2023-12-05 09:17:21 +0700

    first commit

---
+++ src/main/java/connection/ConnectionDB.java
@@ -0,0 +1,31 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    public Connection connect() {
+        Connection connection = null;
+
+        String url = "jdbc:mysql://localhost:3306/db_name";
+        String user = "root";
+        String password = "admin";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection =  DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Connected to the database");
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not load database driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Database connection failed: " + e.getMessage());
+        }
+
+        return connection;
+    }
+}
